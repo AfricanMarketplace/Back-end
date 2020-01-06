ecd7b450cbe5f3795b65a97d3c73a60da4686eb6
Kyle Corbin <kylecorbin26@gmail.com>
2020-01-06 17:26:06 -0500

    work

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,15 @@
 
 module.exports = {
 
-  development: {
+  // development: {
+  //   client: 'sqlite3',
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: './data/dev.sqlite3'
+  //   }
+  // },
+
+  production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations:{
