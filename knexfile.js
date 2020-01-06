26997d94b6ec953933032893c5e7b94ce765e189
Kyle Corbin <kylecorbin26@gmail.com>
2020-01-06 17:18:06 -0500

    please work

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,7 @@
 module.exports = {
 
   development: {
-    client: 'pg',
-    useNullAsDefault: true,
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations:{
       directory: "./data/migrations"
