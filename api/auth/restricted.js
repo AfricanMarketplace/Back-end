dff99aa934471a9742a5515d1351bf96ea6333cb
Kyle Corbin <kylecorbin26@gmail.com>
2020-01-09 16:56:23 -0500

    Changed error code

--- api/auth/restricted.js
+++ api/auth/restricted.js
@@ -7,7 +7,7 @@ module.exports = (req,res, next)=>{
 
     jwt.verify(auth, process.env.TOKEN_SECRET,(err, decoded)=>{
         if(err){
-            res.status(500).json({message:"Token not valid"})
+            res.status(401).json({message:"Token not valid"})
         }else{
             next()
         }
