216d8df306bb87b3bd5ac3fed35922feb3b81105
Kyle Corbin <kylecorbin26@gmail.com>
2020-03-23 01:29:27 -0400

    added the correct error code for when editing a location doesnt work

--- api/location/location-Router.js
+++ api/location/location-Router.js
@@ -44,7 +44,7 @@ router.put("/:id", (req,res)=>{
         res.status(200).json({id})
     })
     .catch(err=>{
-        res.status(200).json({err})
+        res.status(500).json({err})
     })
 })
 
